90996eabc027ac9b4c2bbb7f8a5e5686f71060bb
Alexander Tomashov <sanya-81-chess@mail.ru>
2021-11-30 16:08:37 +0800

    feat: create app

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<div id="app">
+  <p>{{ count }}</p>
+  <p>
+	<button @click="increment">111111111111</button>
+	<button @click="decrement">111111111111</button>
+  </p>
+</div>
+</template>
+
+<script>
+
+export default {
+	name: 'App',
+	computed: {
+	count() {
+		return this.$store.state.count
+	}
+  },
+  methods: {
+	increment() {
+		this.$store.commit('increment')
+	},
+	decrement() {
+		this.$store.commit('decrement')
+	}
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from 'vuex'
+
+export const store = createStore({
+    state: {
+      count: 0
+    },
+    mutations: {
+        increment: state => state.count++,
+        decrement: state => state.count--
+    }
+  })
