098b3ac07455bf09d66d31fdf1a020ceefa9d127
Alexander Tomashov <sanya-81-chess@mail.ru>
2021-12-01 13:40:22 +0800

    feat: add new component BaseTable

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
-<div id="app">
+<div>
   <p>{{ count }}</p>
   <p>
 	<button @click="increment">111111111111</button>
 	<button @click="decrement">111111111111</button>
   </p>
+  <div id="demo">
+      <form id="search">
+        Search <input name="query" v-model="searchQuery" />
+      </form>
+      <BaseTable
+        :heroes="gridData"
+        :columns="gridColumns"
+        :filter-key="searchQuery" 
+      >
+      </BaseTable>
+    </div>
 </div>
 </template>
 
 <script>
-
+import BaseTable from './components/BaseTable.vue'
 export default {
 	name: 'App',
+	components:{BaseTable},
+	data: function() {
+		return {
+          searchQuery: "",
+          gridColumns: ["name", "data", "quantity", "distance"],
+          gridData: this.$store.state.db
+		}
+	},
 	computed: {
 	count() {
 		return this.$store.state.count
@@ -29,5 +48,65 @@ export default {
 </script>
 
 <style>
+body {
+  font-family: Helvetica Neue, Arial, sans-serif;
+  font-size: 14px;
+  color: #444;
+}
+
+table {
+  border: 2px solid #42b983;
+  border-radius: 3px;
+  background-color: #fff;
+}
+
+th {
+  background-color: #42b983;
+  color: rgba(255, 255, 255, 0.66);
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+td {
+  background-color: #f9f9f9;
+}
+
+th,
+td {
+  min-width: 120px;
+  padding: 10px 20px;
+}
+
+th.active {
+  color: #fff;
+}
+
+th.active .arrow {
+  opacity: 1;
+}
+
+.arrow {
+  display: inline-block;
+  vertical-align: middle;
+  width: 0;
+  height: 0;
+  margin-left: 5px;
+  opacity: 0.66;
+}
+
+.arrow.asc {
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
+  border-bottom: 4px solid #fff;
+}
+
+.arrow.dsc {
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
+  border-top: 4px solid #fff;
+}
 
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex'
+import db from '../../db/index.json'
 
 export const store = createStore({
     state: {
-      count: 0
+      count: 0,
+      db,
     },
     mutations: {
         increment: state => state.count++,
