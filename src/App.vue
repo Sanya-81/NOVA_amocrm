268b3d942e1fed514ae3849dff3d9db20d95dd96
Alexander Tomashov <sanya-81-chess@mail.ru>
2021-12-02 11:40:40 +0800

    feat: add new component baseSelect

--- src/App.vue
+++ src/App.vue
@@ -6,26 +6,50 @@
 	Search
 		<input name="query" v-model="searchQuery" />
 	</form>
+	
+
 	<base-table
 	:tebleData="dbData"
 	:thCell="thData"
 	:filter-key="searchQuery" >
 	</base-table>
+<base-select
+		:db="db">
+	</base-select>
 </div>
 </template>
 
 <script>
+import BaseSelect from './components/BaseSelect.vue'
 import BasePagination from './components/BasePagination.vue'
 import BaseTable from './components/BaseTable.vue'
 export default {
 	name: 'App',
-	components:{BaseTable, BasePagination},
+	components:{BaseTable, BasePagination, BaseSelect},
 	data: function() {
 		return {
-          searchQuery: "",
-          thData: ["data", "name", "quantity", "distance"],
-        //   dbData: this.$store.state.data
-        //   dbData:  this.$store.getters.sortTable
+					searchQuery: "",
+					thData: ["data", "name", "quantity", "distance"],
+				//   dbData: this.$store.state.data
+				//   dbData:  this.$store.getters.sortTable
+				db: [
+                {
+                    "value": "E1",
+                    "content": "Русский",
+                },
+                {
+                    "value":"E2",
+                    "content":"Английский",
+                },
+                {
+                    "value":"E3",
+                    "content":"Китайский",
+                },
+                {
+                    "value":"E4",
+                    "content":"Испанский",
+                },
+            ],
 		}
 	},
 	computed: {
@@ -43,65 +67,113 @@ export default {
 </script>
 
 <style>
+   :root {
+    --number-9: 0.25;
+    --number-8: 0.16;
+    --number-7: 0.08;
+    --number-6: 0.0001;
+    --number-5: 0.04;
+    --number-4: 0.02;
+    
+    --white: #ffffff;
+    --blue50: #EBF4F8;
+    --blue100: #DBE2EA;
+    --blue600: #0880AE;
+    --blue800: #756F86;
+    --blue900: #2C2738;
+
+    --black9: rgba(0, 0, 0, var(--number-9));
+    --black8: rgba(44, 39, 56, var(--number-8));
+    --black7: rgba(44, 39, 56, var(--number-7));
+    --black6: rgba(44, 39, 56, var(--number-6));
+    --black5: rgba(44, 39, 56, var(--number-5));
+    --black4: rgba(44, 39, 56, var(--number-4));
+
+    --dropShadowButton: 
+        drop-shadow(0px 4px 4px var(--black9));
+    --dropShadowSelect:
+        drop-shadow(0px 4px 8px var(--black5)),
+        drop-shadow(0px 20px 20px var(--black5));
+    --dropShadowForm:
+        drop-shadow(0px 12px 24px var(--black4)),
+        drop-shadow(0px 32px 64px var(--black5));
+    
+    --dropShadowInput: var(--dropShadowSelect);
+
+    --textShadow: 
+        0px 4px 4px var(--black9);
+    
+    --shadowNormal: 
+        0px 2px 4px var(--black7), 
+        0px 4px 8px var(--black7);
+    --shadowHover: 
+        0px 12px 24px var(--black7),
+        0px 24px 48px var(--black8);
+    --shadowActive: 
+        0px 2px 4px var(--black6), 
+        0px 4px 8px var(--black7);
+    
+    --shadowBorder: var(--blue600) 0 0 0 2px; 
+    }
 body {
-  font-family: Helvetica Neue, Arial, sans-serif;
-  font-size: 14px;
-  color: #444;
+	font-family: Helvetica Neue, Arial, sans-serif;
+	font-size: 14px;
+	color: #444;
 }
 
 table {
-  border: 2px solid #42b983;
-  border-radius: 3px;
-  background-color: #fff;
+	border: 2px solid #42b983;
+	border-radius: 3px;
+	background-color: #fff;
 }
 
 th {
-  background-color: #42b983;
-  color: rgba(255, 255, 255, 0.66);
-  cursor: pointer;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
+	background-color: #42b983;
+	color: rgba(255, 255, 255, 0.66);
+	cursor: pointer;
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	-ms-user-select: none;
+	user-select: none;
 }
 
 td {
-  background-color: #f9f9f9;
+	background-color: #f9f9f9;
 }
 
 th,
 td {
-  min-width: 120px;
-  padding: 10px 20px;
+	min-width: 120px;
+	padding: 10px 20px;
 }
 
 th.active {
-  color: #fff;
+	color: #fff;
 }
 
 th.active .arrow {
-  opacity: 1;
+	opacity: 1;
 }
 
 .arrow {
-  display: inline-block;
-  vertical-align: middle;
-  width: 0;
-  height: 0;
-  margin-left: 5px;
-  opacity: 0.66;
+	display: inline-block;
+	vertical-align: middle;
+	width: 0;
+	height: 0;
+	margin-left: 5px;
+	opacity: 0.66;
 }
 
 .arrow.asc {
-  border-left: 4px solid transparent;
-  border-right: 4px solid transparent;
-  border-bottom: 4px solid #fff;
+	border-left: 4px solid transparent;
+	border-right: 4px solid transparent;
+	border-bottom: 4px solid #fff;
 }
 
 .arrow.dsc {
-  border-left: 4px solid transparent;
-  border-right: 4px solid transparent;
-  border-top: 4px solid #fff;
+	border-left: 4px solid transparent;
+	border-right: 4px solid transparent;
+	border-top: 4px solid #fff;
 }
 
 </style>
---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,301 @@
+<template>
+<div>
+    <p class="selectLabel" id="jobLabel">Язык</p>
+    <div class="selectWrapper">
+        <select class="
+                    selectNative 
+                    js-selectNative
+                " 
+                aria-labelledby="jobLabel"
+                @change= changeNative($event)
+                ref='native'
+        >   <option 
+                value="sel"         
+                disabled 
+                selected
+                ref="option"
+            > {{elSelectState}} </option>
+            <option 
+                v-for = "obj of db"
+                :key  = "obj.value"
+                :value = obj.value
+            > {{obj.content}} </option>    
+            
+        </select>
+        <div 
+            :class="{isActive: Active}"
+            class="
+                selectCustom 
+                js-selectCustom
+            " 
+            :aria-hidden = ariaHidden
+        ><div
+            @click = openSelectCustom()
+            class="selectCustom-trigger"
+                ref='closeSelectRoot'
+            > {{elSelectState}}</div>
+            <div 
+                class="selectCustom-options"
+            >
+                <div
+                    v-for = "(obj,index) of db"
+                    :key  = obj.value
+                    :class="[
+                        {isActive: elSelectState === obj.content}, 
+                        {isHover: optionChecked === obj.value}
+                    ]"
+                    class = "selectCustom-option"
+                    :data-value = obj.value
+                    @mouseenter = "
+                        optionChecked = obj.value, 
+                        optionIndex = index
+                    "
+                    @click = "
+                        elSelectState = obj.content, 
+                        changeCustom($event),
+                        closeSelectCustom()
+                    "
+                >{{obj.content}}</div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        db: {
+            type: Array,
+        }
+    },
+    data() {
+        return {
+            Active: false,
+            ariaHidden: 'true',
+            optionChecked: null,
+            optionIndex: -1,
+            value: '',
+            content: '',
+            'data-value': '',
+            elSelectState: 'Язык',
+        }
+    },
+    
+    methods: {
+        changeNative(event) {
+            const nativeValue = event.target.value;
+            const proxy = this.db.filter((obj, index) => {
+                this.optionIndex = index;
+                return obj.value === nativeValue}); 
+            this.elSelectState =  proxy[0].content
+            this.optionChecked = nativeValue
+        },
+
+        changeCustom(event) {
+            this.$refs.native.value = event.target.getAttribute("data-value")
+            console.log(event.target)
+        },
+
+        openSelectCustom() {
+            this.Active = !this.Active
+            this.ariaHidden = 'false';
+        },
+
+        closeSelectCustom() {
+            this.ariaHidden = 'true';
+            this.Active = false
+        },
+        
+        closeSelectRoot(e) {
+            const el = this.$refs.closeSelectRoot;
+            const target = e.target;
+            if  (el != target && !el.contains(target)) {
+                this.Active = false
+            }
+        },
+        
+        currentOptionValue() {
+            const optionValue = this.db[this.optionIndex].value
+            this.optionChecked = optionValue
+        },
+
+        KeyboardNavigation(e) {
+            if(this.Active && e.keyCode === 40){
+                e.preventDefault();
+                
+                if(this.optionIndex != this.db.length - 1){
+                    this.optionIndex++;
+                    this.currentOptionValue();
+                } 
+            }
+
+            if(this.Active && e.keyCode === 38){
+                e.preventDefault();
+                
+                if(this.optionIndex > 0){
+                    this.optionIndex--;
+                    this.currentOptionValue();
+                }
+            }
+
+            if(this.Active && e.keyCode === 13 || e.keyCode === 32){
+                this.$refs.native.value = this.db[this.optionIndex].value
+                this.elSelectState = this.db[this.optionIndex].content
+                this.closeSelectCustom()
+            }
+
+            if(this.Active && e.keyCode === 27){
+                this.closeSelectCustom()
+            }
+        }
+    },
+
+    created() {
+        document.addEventListener('click', this.closeSelectRoot)
+        document.addEventListener('keydown', this.KeyboardNavigation)
+    },
+    destroy() {
+        document.removeEventListener('click', this.closeSelectRoot)
+        document.removeEventListener('keydown', this.KeyboardNavigation)
+    }
+}
+</script>
+
+<style>
+    .selectNative,
+    .selectCustom {
+        position: relative;
+    }
+
+    .selectCustom {
+    position: absolute;
+    width: 100%;
+    top:  0;
+    left: 0;
+    display: none;
+    }
+
+    @media (hover: hover) {
+        .selectCustom {
+            display: block;
+        }
+ 
+        .selectNative:focus + .selectCustom {
+            display: none;
+        }
+    }
+
+    .selectNative:focus,
+    .selectCustom.isActive .selectCustom-trigger {
+        outline: none;
+        box-shadow:
+            var(--shadowBorder),
+            var(--shadowActive);
+    }
+
+    .selectLabel {
+        display: block;
+        margin-bottom: 0.4rem;
+        color: var(--blue800);
+    }
+
+    .selectWrapper {
+        position: relative;
+    }
+
+    .selectNative,
+    .selectCustom-trigger {
+        --font:    16px;
+        /* --width:  360px; */
+        --height:  52px;
+
+        width: 100%;
+        /* height: var(--height); */
+        padding: calc((var(--height) / 2) - var(--font));
+        padding-left: 16px;
+        border-radius: 6px;
+        box-shadow: var(--blue100) 0 0 0 2px;
+
+        background-color: var(--white);
+        font-size: var(--font);
+        line-height: 2;
+    }
+
+    .selectNative {
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        border-style: none;
+        background: url('../../public/dropDown.svg');
+        background-color: var(--white);
+        background-repeat: no-repeat;
+        background-position-x: 95%;
+        background-position-y: 50%;
+    }
+
+    .selectCustom-trigger { 
+        position: relative;
+        filter: var(--dropShadowSelect);
+        cursor: pointer;
+    }
+
+    .selectCustom-trigger::after {
+        content: url('../../public/dropDown.svg');
+        position: absolute;
+        top: -65%;
+        right: 5%;
+    }
+
+    .selectCustom-trigger:hover {
+        box-shadow: 
+        var(--shadowBorder),
+        var(--shadowHover);
+
+    }
+
+    .selectCustom-options {
+        position: absolute;
+        display: none;
+        width:  100%;
+        margin-top: 4px;
+        padding: 12px 0;
+        border-radius: 6px;
+        box-shadow: var(--shadowNormal);
+        background-color: var(--white);
+        color: var(--900);
+        z-index: 1;
+    }
+
+    .selectCustom.isActive .selectCustom-options {
+        display: block;
+    }
+
+    .selectCustom-option {
+        position: relative;
+        padding: 0.8rem;
+        padding-left: 16px;
+        color: var(--blue800);
+        /* background-color: var(--white); */
+    }
+
+    .selectCustom-option.isHover,
+    .selectCustom-option:hover {
+        background-color: var(--blue50);
+        cursor: default;
+    }
+
+    .selectCustom-option:not(:last-of-type)::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        border-bottom: 1px solid var(--blue100);
+    }
+
+    .selectCustom-option.isActive::before {
+        content: "✓";
+        position: absolute;
+        right: 5%;
+    }
+</style>
