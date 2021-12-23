ff4519ec6f88001e1a592d431384d5d070d0cda6
Alexander Tomashov <sanya-81-chess@mail.ru>
2021-12-23 13:24:18 +0800

    refactore: main.js add normalize and  root-*

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import {store}  from './store/index.js'
+import 'normalize.css'
+
+import '../../root-color.css'
+import '../../root-shadow.css'
 
 createApp(App).use(store).mount('#app')
